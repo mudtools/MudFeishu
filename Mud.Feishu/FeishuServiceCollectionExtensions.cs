0934d822c49ea8afb9ebc4b65b10d5791f527fd5
倔强的石头 <413901125@qq.com>
2025-11-13 16:56:34 +0800

    添加飞书服务注册扩展方法。

---
+++ Mud.Feishu/FeishuServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿
+using Mud.Feishu;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// 飞书相关服务注册扩展方法
+/// </summary>
+public static class FeishuServiceCollectionExtensions
+{
+    /// <summary>
+    /// 注册飞书 API 服务
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddFeishuApiService(this IServiceCollection services)
+    {
+        return services.AddWebApiHttpClient();
+    }
+}

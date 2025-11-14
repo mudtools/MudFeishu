5ec806c35de7aa18ed06835828dc2fdf03204ee4
倔强的石头 <413901125@qq.com>
2025-11-14 17:25:16 +0800

    为人员类型实体添加注释。

--- Mud.Feishu/DataModels/EmployeeType/EmployeeTypeEnum.cs
+++ Mud.Feishu/DataModels/EmployeeType/EmployeeTypeEnum.cs
@@ -5,21 +5,43 @@
 /// </summary>
 public class EmployeeTypeEnum
 {
+    /// <summary>
+    /// 枚举 ID。
+    /// </summary>
     [JsonPropertyName("enum_id")]
-    public string EnumId { get; set; }
+    public string? EnumId { get; set; }
 
+    /// <summary>
+    /// 枚举值。
+    /// </summary>
     [JsonPropertyName("enum_value")]
-    public string EnumValue { get; set; }
+    public string? EnumValue { get; set; }
 
+    /// <summary>
+    /// 枚举内容。
+    /// </summary>
     [JsonPropertyName("content")]
-    public string Content { get; set; }
+    public string? Content { get; set; }
 
+    /// <summary>
+    /// 枚举类型。
+    /// <para>0：系统预定义枚举</para>
+    /// <para>1：用户自定义枚举</para>
+    /// </summary>
     [JsonPropertyName("enum_type")]
     public int EnumType { get; set; }
 
+    /// <summary>
+    /// 枚举状态。
+    /// <para>0：已停用</para>
+    /// <para>1：启用</para>
+    /// </summary>
     [JsonPropertyName("enum_status")]
     public int EnumStatus { get; set; }
 
+    /// <summary>
+    /// 枚举内容的国际化配置。
+    /// </summary>
     [JsonPropertyName("i18n_content")]
-    public List<I18nContent> I18nContent { get; set; }
+    public List<I18nContent>? I18nContent { get; set; }
 }

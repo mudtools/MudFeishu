123f43f3714fc54a01c282ebd4c97946e3bac98f
倔强的石头 <413901125@qq.com>
2025-11-14 17:31:01 +0800

    添加更新部门的自定义 ID函数。

---
+++ Mud.Feishu/DataModels/Departments/RequestModel/DepartMentUpdateIdRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Mud.Feishu.DataModels.Departments;
+
+/// <summary>
+/// 更新部门 ID请求体。
+/// </summary>
+public class DepartMentUpdateIdRequest
+{
+    /// <summary>
+    /// 新的自定义部门 ID
+    /// </summary>
+    [JsonPropertyName("new_department_id")]
+    public required string NewDepartmentId { get; set; }
+}
--- Mud.Feishu/IFeishuDepartmentsApi.cs
+++ Mud.Feishu/IFeishuDepartmentsApi.cs
@@ -20,12 +20,12 @@ public interface IFeishuDepartmentsApi
     /// <returns></returns>
     [Post("https://open.feishu.cn/open-apis/contact/v3/departments")]
     Task<FeishuApiResult<DepartmentCreateUpdateResult>> CreateDepartmentAsync(
-       [Header("Authorization")] string user_access_token,
-       [Body] DepartmentCreateRequest departmentCreateRequest,
-       [Query("user_id_type")] string? user_id_type = null,
-       [Query("department_id_type")] string? department_id_type = null,
-       [Query("client_token")] string? client_token = null,
-       CancellationToken cancellationToken = default);
+           [Header("Authorization")] string user_access_token,
+           [Body] DepartmentCreateRequest departmentCreateRequest,
+           [Query("user_id_type")] string? user_id_type = null,
+           [Query("department_id_type")] string? department_id_type = null,
+           [Query("client_token")] string? client_token = null,
+           CancellationToken cancellationToken = default);
 
     /// <summary>
     /// 更新指定部门的部分信息，包括名称、父部门、排序以及负责人等。
@@ -38,12 +38,12 @@ public interface IFeishuDepartmentsApi
     /// <param name="cancellationToken"><see cref="CancellationToken"/>对象。</param>
     [Patch("https://open.feishu.cn/open-apis/contact/v3/departments/{department_id}")]
     Task<FeishuApiResult<DepartmentCreateUpdateResult>> UpdatePartDepartmentAsync(
-      [Header("Authorization")] string user_access_token,
-      [Path] string department_id,
-      [Body] DepartmentPartUpdateRequest departmentCreateRequest,
-      [Query("user_id_type")] string? user_id_type = null,
-      [Query("department_id_type")] string? department_id_type = null,
-      CancellationToken cancellationToken = default);
+          [Header("Authorization")] string user_access_token,
+          [Path] string department_id,
+          [Body] DepartmentPartUpdateRequest departmentCreateRequest,
+          [Query("user_id_type")] string? user_id_type = null,
+          [Query("department_id_type")] string? department_id_type = null,
+          CancellationToken cancellationToken = default);
 
     /// <summary>
     /// 更新指定部门的信息，包括名称、父部门以及负责人等信息。
@@ -54,12 +54,30 @@ public interface IFeishuDepartmentsApi
     /// <param name="user_id_type">用户 ID 类型</param>
     /// <param name="department_id_type">此次调用中的部门 ID 类型。</param>
     /// <param name="cancellationToken"><see cref="CancellationToken"/>对象。</param>
-    [Patch("https://open.feishu.cn/open-apis/contact/v3/departments/{department_id}")]
+    [Put("https://open.feishu.cn/open-apis/contact/v3/departments/{department_id}")]
     Task<FeishuApiResult<DepartmentUpdateResult>> UpdateDepartmentAsync(
-     [Header("Authorization")] string user_access_token,
-     [Path] string department_id,
-     [Body] DepartmentUpdateRequest departmentCreateRequest,
-     [Query("user_id_type")] string? user_id_type = null,
-     [Query("department_id_type")] string? department_id_type = null,
-     CancellationToken cancellationToken = default);
+         [Header("Authorization")] string user_access_token,
+         [Path] string department_id,
+         [Body] DepartmentUpdateRequest departmentCreateRequest,
+         [Query("user_id_type")] string? user_id_type = null,
+         [Query("department_id_type")] string? department_id_type = null,
+         CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// 更新部门的自定义 ID，即 department_id。
+    /// </summary>
+    /// <param name="user_access_token">应用调用 API 时，通过访问凭证（access_token）进行身份鉴权</param>
+    /// <param name="departMentUpdateIdRequest">更新部门ID的请求体。</param>
+    /// <param name="department_id">部门 ID，ID 类型需要与查询参数 department_id_type 的取值保持一致。</param>
+    /// <param name="department_id_type">此次调用中的部门 ID 类型。</param>
+    /// <param name="cancellationToken"><see cref="CancellationToken"/>对象。</param>
+    [Patch("https://open.feishu.cn/open-apis/contact/v3/departments/{department_id}/update_department_id")]
+    Task<FeishuNullDataApiResult> UpdateDepartmentIdAsync(
+        [Header("Authorization")] string user_access_token,
+        [Path] string department_id,
+        [Body] DepartMentUpdateIdRequest departMentUpdateIdRequest,
+        [Query("department_id_type")] string? department_id_type = null,
+        CancellationToken cancellationToken = default);
+
+
 }

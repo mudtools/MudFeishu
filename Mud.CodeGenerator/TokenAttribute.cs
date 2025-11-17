2f9accdd54fbe76621934601a8e3d3b49ec11137
倔强的石头 <413901125@qq.com>
2025-11-17 19:34:50 +0800

    添加TokenType特性。

--- Mud.CodeGenerator/TokenAttribute.cs
+++ Mud.CodeGenerator/TokenAttribute.cs
@@ -4,12 +4,45 @@
 /// HTTP 声明式token参数特性
 /// </summary>
 [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false)]
-public sealed class TokenAttribute : Attribute
+internal sealed class TokenAttribute : Attribute
 {
     /// <summary>
-    /// <inheritdoc cref="PathAttribute" />
+    /// <inheritdoc cref="TokenAttribute" />
     /// </summary>
-    public TokenAttribute()
+    public TokenAttribute() : this(TokenType.TenantAccessToken)
     {
     }
+
+    /// <summary>
+    /// <inheritdoc cref="TokenAttribute" />
+    /// </summary>
+    /// <param name="tokenType">飞书Token类型。</param>
+    public TokenAttribute(TokenType tokenType)
+    {
+        TokenType = tokenType;
+    }
+
+    /// <summary>
+    /// 飞书Token类型。
+    /// </summary>
+    public TokenType TokenType { get; set; } = TokenType.TenantAccessToken;
+}
+
+/// <summary>
+/// 飞书Token类型。
+/// </summary>
+internal enum TokenType
+{
+    /// <summary>
+    /// 使用应用Token调用函数。
+    /// </summary>
+    TenantAccessToken = 0,
+    /// <summary>
+    /// 使用用户Token调用函数。
+    /// </summary>
+    UserAccessToken = 1,
+    /// <summary>
+    /// 由用户决定使用何种Token调用函数。
+    /// </summary>
+    Both = 2,
 }

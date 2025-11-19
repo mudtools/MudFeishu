665312ae10c2901f88151ffceeec1f0dafe4ad1f
倔强的石头 <413901125@qq.com>
2025-11-19 09:30:47 +0800

    添加飞书职务相关API函数。

---
+++ Mud.Feishu/DataModels/JobTitles/JobTitle.cs
@@ -0,0 +1,16 @@
+﻿namespace Mud.Feishu.DataModels.JobTitles;
+
+public class JobTitle
+{
+    [JsonPropertyName("job_title_id")]
+    public string JobTitleId { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("i18n_name")]
+    public List<I18nContent> I18nName { get; set; }
+
+    [JsonPropertyName("status")]
+    public bool Status { get; set; }
+}

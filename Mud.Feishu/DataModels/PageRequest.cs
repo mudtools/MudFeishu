71d0ea22d37cf52c8daa40a0ffc24c26783534e8
倔强的石头 <413901125@qq.com>
2025-11-20 09:07:23 +0800

    完成IFeishuV1EmployeesApi中所有函数。

---
+++ Mud.Feishu/DataModels/PageRequest.cs
@@ -0,0 +1,26 @@
+﻿// -----------------------------------------------------------------------
+//  作者：Mud Studio  版权所有 (c) Mud Studio 2025   
+//  Mud.Feishu 项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
+//  本项目主要遵循 MIT 许可证进行分发和使用。许可证位于源代码树根目录中的 LICENSE-MIT 文件。
+//  不得利用本项目从事危害国家安全、扰乱社会秩序、侵犯他人合法权益等法律法规禁止的活动！任何基于本项目开发而产生的一切法律纠纷和责任，我们不承担任何责任！
+// -----------------------------------------------------------------------
+
+namespace Mud.Feishu.DataModels;
+
+/// <summary>
+/// 分页参数
+/// </summary>
+public class PageRequest
+{
+    /// <summary>
+    /// 本次请求条数，最小0条，最大100条
+    /// </summary>
+    [JsonPropertyName("page_size")]
+    public int? PageSize { get; set; }
+
+    /// <summary>
+    /// 分页标记，第一次请求不填，表示从头开始遍历；分页查询结果还有更多项时会同时返回新的 page_token，下次遍历可采用该 page_token 获取查询结果
+    /// </summary>
+    [JsonPropertyName("page_token")]
+    public string? PageToken { get; set; }
+}
--- Mud.Feishu/IFeishuV1EmployeesApi.cs
+++ Mud.Feishu/IFeishuV1EmployeesApi.cs
@@ -144,4 +144,38 @@ public interface IFeishuV1EmployeesApi
         [Query("employee_id_type")] string? employee_id_type = Consts.User_Id_Type,
         [Query("department_id_type")] string? department_id_type = Consts.Department_Id_Type,
         CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// 用于依据指定条件，分页批量获取符合条件的员工详情列表。
+    /// <para>员工指飞书企业内身份为「Employee」的成员，等同于通讯录OpenAPI中的「User」</para> 
+    /// </summary>
+    /// <param name="access_token">应用调用 API 时，需要通过访问凭证（access_token）进行身份鉴权</param>
+    /// <param name="employeeQueryRequest">员工查询请求体。</param>
+    /// <param name="employee_id_type">用户 ID 类型</param>
+    /// <param name="department_id_type">此次调用中的部门 ID 类型。</param>
+    /// <param name="cancellationToken"><see cref="CancellationToken"/>取消操作令牌对象。</param>
+    [Post("https://open.feishu.cn/open-apis/directory/v1/employees/filter")]
+    Task<FeishuApiResult<EmployeeListPageResult>> QueryEmployeePageListAsync(
+       [Token(TokenType.Both)][Header("Authorization")] string access_token,
+       [Body] EmployeeSearchRequest employeeQueryRequest,
+       [Query("employee_id_type")] string? employee_id_type = Consts.User_Id_Type,
+       [Query("department_id_type")] string? department_id_type = Consts.Department_Id_Type,
+       CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// 用于搜索员工信息，如通过关键词搜索员工的名称、手机号、邮箱等信息。
+    /// <para>员工指飞书企业内身份为「Employee」的成员，等同于通讯录OpenAPI中的「User」</para> 
+    /// </summary>
+    /// <param name="access_token">应用调用 API 时，需要通过访问凭证（access_token）进行身份鉴权</param>
+    /// <param name="employeeQueryRequest">员工查询请求体。</param>
+    /// <param name="employee_id_type">用户 ID 类型</param>
+    /// <param name="department_id_type">此次调用中的部门 ID 类型。</param>
+    /// <param name="cancellationToken"><see cref="CancellationToken"/>取消操作令牌对象。</param>
+    [Post("https://open.feishu.cn/open-apis/directory/v1/employees/search")]
+    Task<FeishuApiResult<EmployeeListPageResult>> SearchEmployeePageListAsync(
+      [Token(TokenType.Both)][Header("Authorization")] string access_token,
+      [Body] EmployeePageQueryRequest employeeQueryRequest,
+      [Query("employee_id_type")] string? employee_id_type = Consts.User_Id_Type,
+      [Query("department_id_type")] string? department_id_type = Consts.Department_Id_Type,
+      CancellationToken cancellationToken = default);
 }

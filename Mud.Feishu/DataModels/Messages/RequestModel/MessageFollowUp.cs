67e9530b575daac7690f9b08ba4640cd16b076dd
倔强的石头 <413901125@qq.com>
2025-11-21 22:19:45 +0800

    添加查询指定消息是否已读接口函数。

---
+++ Mud.Feishu/DataModels/Messages/RequestModel/MessageFollowUp.cs
@@ -0,0 +1,31 @@
+﻿// -----------------------------------------------------------------------
+//  作者：Mud Studio  版权所有 (c) Mud Studio 2025   
+//  Mud.Feishu 项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
+//  本项目主要遵循 MIT 许可证进行分发和使用。许可证位于源代码树根目录中的 LICENSE-MIT 文件。
+//  不得利用本项目从事危害国家安全、扰乱社会秩序、侵犯他人合法权益等法律法规禁止的活动！任何基于本项目开发而产生的一切法律纠纷和责任，我们不承担任何责任！
+// -----------------------------------------------------------------------
+
+namespace Mud.Feishu.DataModels.Messages;
+
+/// <summary>
+/// 跟随气泡
+/// </summary>
+public class MessageFollowUp
+{
+    /// <summary>
+    /// 气泡的内容。示例值："你好"
+    /// </summary>
+    [JsonPropertyName("content")]
+    public
+#if NET7_0_OR_GREATER
+        required
+#endif
+        string? Content
+    { get; set; }
+
+    /// <summary>
+    /// 气泡的多语言内容。
+    /// </summary>
+    [JsonPropertyName("i18n_contents")]
+    public List<I18nContentLanguage>? I18nContents { get; set; }
+}
--- Mud.Feishu/Messages/IFeishuV1MessageApi.cs
+++ Mud.Feishu/Messages/IFeishuV1MessageApi.cs
@@ -119,4 +119,53 @@ public interface IFeishuV1MessageApi
        [Query("receive_id_type")] string receive_id_type = Consts.User_Id_Type,
        [Query("uuid")] string? uuid = null,
        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// 撤回指定消息。调用接口的身份不同（身份通过 Authorization 请求头参数指定），可实现的效果不同：
+    /// <para> 机器人可以撤回该机器人自己发送的消息。</para>
+    /// <para> 群聊的群主可以撤回群内指定的消息。</para>
+    /// </summary>
+    /// <param name="access_token">应用调用 API 时，通过访问凭证（access_token）进行身份鉴权</param>
+    /// <param name="message_id">待撤回的消息 ID。示例值："om_dc13264520392913993dd051dba21dcf"</param>
+    /// <param name="cancellationToken"><see cref="CancellationToken"/>取消操作令牌对象。</param>
+    /// <returns></returns>
+    [Delete("https://open.feishu.cn/open-apis/im/v1/messages/{message_id}")]
+    Task<FeishuNullDataApiResult> RevokeMessageAsync(
+        [Token(TokenType.Both)][Header("Authorization")] string access_token,
+        [Path] string message_id,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// 在最新一条消息下方添加气泡样式的内容，当消息接收者点击气泡或者新消息到达后，气泡消失。
+    /// </summary>
+    /// <param name="tenant_access_token">应用调用 API 时，通过访问凭证（access_token）进行身份鉴权</param>
+    /// <param name="message_id">机器人发送的消息 ID。示例值："om_dc13264520392913993dd051dba21dcf"</param>
+    /// <param name="messageFollowUpRequest">跟随气泡请求体。</param>
+    /// <param name="cancellationToken"><see cref="CancellationToken"/>取消操作令牌对象。</param>
+    /// <returns></returns>
+    [Post("https://open.feishu.cn/open-apis/im/v1/messages/{message_id}/push_follow_up")]
+    Task<FeishuNullDataApiResult> CreateMessageFollowUpAsync(
+      [Token][Header("Authorization")] string tenant_access_token,
+      [Path] string message_id,
+      [Body] MessageFollowUpRequest messageFollowUpRequest,
+      CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// 查询指定消息是否已读。接口只返回已读用户的信息，不返回未读用户的信息。
+    /// </summary>
+    /// <param name="tenant_access_token">应用调用 API 时，需要通过访问凭证（access_token）进行身份鉴权</param>
+    /// <param name="user_id_type">用户 ID 类型</param>
+    /// <param name="cancellationToken"><see cref="CancellationToken"/>取消操作令牌对象。</param>
+    /// <param name="message_id">待查询的消息 ID。</param>
+    /// <param name="page_size">分页大小，即本次请求所返回的用户信息列表内的最大条目数。默认值：10</param>
+    /// <param name="page_token">分页标记，第一次请求不填，表示从头开始遍历；分页查询结果还有更多项时会同时返回新的 page_token，下次遍历可采用该 page_token 获取查询结果</param>
+    /// <returns></returns>
+    [Get("https://open.feishu.cn/open-apis/im/v1/messages/{message_id}/read_users")]
+    Task<FeishuApiListResult<ReadMessageUser>> GetMessageReadUsesAsync(
+     [Token][Header("Authorization")] string tenant_access_token,
+     [Path] string message_id,
+     [Query("page_size")] int page_size = 10,
+     [Query("page_token")] string? page_token = null,
+     [Query("user_id_type")] string? user_id_type = Consts.User_Id_Type,
+     CancellationToken cancellationToken = default);
 }

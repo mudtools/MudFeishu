e0b374061fa3d57bcb93d1202fd74a5f4470d1fe
倔强的石头 <413901125@qq.com>
2026-04-02 14:21:11 +0800

    fix: 更新依赖版本并添加请求体加密方法
    更新 Mud.HttpUtils 和 Mud.HttpUtils.Generator 到 1.6.4 版本
    在 FeishuHttpClient 中添加未实现的 EncryptContent 方法

--- Mud.Feishu.Abstractions/Extensions/FeishuHttpClient.cs
+++ Mud.Feishu.Abstractions/Extensions/FeishuHttpClient.cs
@@ -1,5 +1,5 @@
 // -----------------------------------------------------------------------
-//  作者：Mud Studio  版权所有 (c) Mud Studio 2025   
+//  作者：Mud Studio  版权所有 (c) Mud Studio 2026   
 //  Mud.Feishu 项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
 //  本项目主要遵循 MIT 许可证进行分发和使用。许可证位于源代码树根目录中的 LICENSE-MIT 文件。
 //  不得利用本项目从事危害国家安全、扰乱社会秩序、侵犯他人合法权益等法律法规禁止的活动！任何基于本项目开发而产生的一切法律纠纷和责任，我们不承担任何责任！
@@ -30,5 +30,11 @@ internal class FeishuHttpClient : EnhancedHttpClient, IEnhancedHttpClient
     protected override JsonSerializerOptions? GetJsonSerializerOptions()
     {
         return _jsonSerializerOptionsMonitor.CurrentValue;
+    }
+
+    public override string EncryptContent(object content, string propertyName = "data", SerializeType serializeType = SerializeType.Json)
+    {
+        _logger.LogWarning("当前类库无须使用请求体加密功能。");
+        throw new NotImplementedException("当前类库无须使用请求体加密功能。");
     }
 }

81d57f9ad9b807a0a29a9a6d4a41688a1c23d387
倔强的石头 <413901125@qq.com>
2026-04-25 14:47:01 +0800

    feat(calendar): 添加搜索日历接口

--- Mud.Feishu/Interfaces/Calendar/IFeishuV4Calendar.cs
+++ Mud.Feishu/Interfaces/Calendar/IFeishuV4Calendar.cs
@@ -227,4 +227,21 @@ public interface IFeishuV4Calendar : IFeishuAppContextSwitcher
         [Path] string calendar_id,
         [Body] UpdateCalendarRequest updateCalendarRequest,
         CancellationToken cancellationToken = default);
+
+
+    /// <summary>
+    /// 搜索日历
+    /// <para>通过关键字搜索日历，搜索结果为标题或描述包含关键字的公共日历或用户主日历。</para>
+    /// <para><see href="https://open.feishu.cn/document/server-docs/calendar-v4/calendar/search">接口文档</see></para>
+    /// </summary>
+    /// <param name="searchCalendarsRequest">搜索日历请求体</param>
+    /// <param name="page_size">分页大小，即本次请求所返回的信息列表内的最大条目数。默认值：500</param>
+    /// <param name="page_token">分页标记，第一次请求不填，表示从头开始遍历；分页查询结果还有更多项时会同时返回新的 page_token，下次遍历可采用该 page_token 获取查询结果</param>
+    /// <param name="cancellationToken"><see cref="CancellationToken"/>取消操作令牌对象。</param>
+    [Post("/open-apis/calendar/v4/calendars/search")]
+    Task<FeishuApiPageListResult<CalendarInfo>?> SearchCalendarsPageListAsync(
+        [Body] SearchCalendarsRequest searchCalendarsRequest,
+        [Query] int page_size = Consts.PageSize_500,
+        [Query] string? page_token = null,
+        CancellationToken cancellationToken = default);
 }

d5449da5843e51203e1ceb27c8dd8c679482b45a
shiliu <982797454@qq.com>
2026-05-01 14:30:51 +0800

    build: 更新项目版本和依赖至3.0.0-preview1和2.0.0-preview4
    更新Directory.Build.props中的版本号至3.0.0-preview1
    更新Mud.HttpUtils相关依赖至2.0.0-preview4
    在UserTokenManager中添加空值检查

--- Mud.Feishu.Abstractions/Authentication/TokenManager/UserTokenManager.cs
+++ Mud.Feishu.Abstractions/Authentication/TokenManager/UserTokenManager.cs
@@ -2,7 +2,7 @@
 //  作者：Mud Studio  版权所有 (c) Mud Studio 2026   
 //  Mud.Feishu 项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
 //  本项目主要遵循 MIT 许可证进行分发和使用。许可证位于源代码树根目录中的 LICENSE-MIT 文件。
-//  不得利用本项目从事危害国家安全、扰乱社会秩序、侵犯他人合法权益等法律法规禁止的活动！
+//  不得利用本项目从事危害国家安全、扰乱社会秩序、侵犯他人合法权益等法律法规禁止的活动！任何基于本项目开发而产生的一切法律纠纷和责任，我们不承担任何责任！
 // -----------------------------------------------------------------------
 
 using Microsoft.Extensions.Options;
@@ -276,12 +276,12 @@ internal class UserTokenManager : UserTokenManagerBase, IFeishuUserTokenManager
             var remainingSeconds = (tokenInfo.AccessTokenExpireTime - DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()) / 1000L;
             if (remainingSeconds > 0)
             {
-                await _userTokenStore.SetAccessTokenAsync(userId, _tokenTypeKey, tokenInfo.AccessToken, remainingSeconds, cancellationToken).ConfigureAwait(false);
+                await _userTokenStore.SetAccessTokenAsync(userId, _tokenTypeKey, tokenInfo.AccessToken!, remainingSeconds, cancellationToken).ConfigureAwait(false);
             }
 
             if (!string.IsNullOrEmpty(tokenInfo.RefreshToken))
             {
-                await _userTokenStore.SetRefreshTokenAsync(userId, _tokenTypeKey, tokenInfo.RefreshToken, cancellationToken).ConfigureAwait(false);
+                await _userTokenStore.SetRefreshTokenAsync(userId, _tokenTypeKey, tokenInfo.RefreshToken!, cancellationToken).ConfigureAwait(false);
             }
         }
         catch (Exception ex)

27b2add01234ca5b2142cd74e82be8bc4392ce50
倔强的石头 <413901125@qq.com>
2026-05-20 16:34:22 +0800

    feat(feishu-vc): add paging meeting room list query api
    add the GetMeetingRoomsPageListAsync method to support paging query meeting rooms under specified level or tenant, refer to feishu open api docs

--- Mud.Feishu/Interfaces/VideoConferencing/IFeishuV1VideoConferencingRoom_Tenant.cs
+++ Mud.Feishu/Interfaces/VideoConferencing/IFeishuV1VideoConferencingRoom_Tenant.cs
@@ -109,4 +109,34 @@ public interface IFeishuTenantV1VideoConferencingRoom
       [Body] GetMeetingRoomsRequest getMeetingRoomsRequest,
       [Query] string? user_id_type = Consts.User_Id_Type,
       CancellationToken cancellationToken = default);
+
+
+    /// <summary>
+    /// 分页查询会议室列表
+    /// <para>可以用来查询某个会议室层级下会议室列表。</para>
+    /// <para><see href="https://open.feishu.cn/document/server-docs/vc-v1/room/list">接口文档</see></para>
+    /// </summary>
+    /// <param name="room_level_id">
+    /// <para>层级ID，当需要获取租户下会议室列表时，room_level_id可传空</para>
+    /// <para>示例值：omb_4ad1a2c7a2fbc5fc9570f38456931293</para>
+    /// </param>
+    /// <param name="user_id_type">
+    /// <para>用户 ID 类型</para>
+    /// <para>示例值：open_id</para>
+    /// <list type="bullet">
+    /// <item>open_id：标识一个用户在某个应用中的身份。同一个用户在不同应用中的 Open ID 不同。</item>
+    /// <item>union_id：标识一个用户在某个应用开发商下的身份。同一用户在同一开发商下的应用中的 Union ID 是相同的，在不同开发商下的应用中的 Union ID 是不同的。通过 Union ID，应用开发商可以把同个用户在多个应用中的身份关联起来。</item>
+    /// <item>user_id：标识一个用户在某个租户内的身份。同一个用户在租户 A 和租户 B 内的 User ID 是不同的。在同一个租户内，一个用户的 User ID 在所有应用（包括商店应用）中都保持一致。User ID 主要用于在不同的应用间打通用户数据。</item>
+    /// </list>
+    /// </param>
+    /// <param name="page_size">分页大小，即本次请求所返回的列表内的最大条目数。默认值：10</param>
+    /// <param name="page_token">分页标记，第一次请求不填，表示从头开始遍历；分页查询结果还有更多项时会同时返回新的 page_token，下次遍历可采用该 page_token 获取查询结果</param>
+    /// <param name="cancellationToken"><see cref="CancellationToken"/>取消操作令牌对象。</param>
+    [Get("/open-apis/vc/v1/rooms")]
+    Task<FeishuApiPageListResult<MeetingRoomInfo>?> GetMeetingRoomsPageListAsync(
+      [Query] string? room_level_id = null,
+      [Query] int page_size = Consts.PageSize_10,
+      [Query] string? page_token = null,
+      [Query] string? user_id_type = Consts.User_Id_Type,
+      CancellationToken cancellationToken = default);
 }

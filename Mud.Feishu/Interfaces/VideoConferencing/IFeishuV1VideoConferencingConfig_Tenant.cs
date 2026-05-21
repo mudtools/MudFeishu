feb8c5b688bd5845144e1d2a5ad7f2cf665d2487
倔强的石头 <413901125@qq.com>
2026-05-21 09:56:14 +0800

    feat(video-conferencing): add update reserve config form api support
    add the request model and api interface for updating meeting room reserve config form

--- Mud.Feishu/Interfaces/VideoConferencing/IFeishuV1VideoConferencingConfig_Tenant.cs
+++ Mud.Feishu/Interfaces/VideoConferencing/IFeishuV1VideoConferencingConfig_Tenant.cs
@@ -172,4 +172,32 @@ public interface IFeishuTenantV1VideoConferencingConfig : IFeishuAppContextSwitc
         [Query] string scope_type,
         [Query] string? user_id_type = Consts.User_Id_Type,
         CancellationToken cancellationToken = default);
+
+
+
+    /// <summary>
+    /// 更新会议室预定表单。
+    /// <para><see href="https://open.feishu.cn/document/server-docs/vc-v1/scope_config/patch-2">接口文档</see></para>
+    /// </summary>   
+    /// <param name="user_id_type">
+    /// <para>用户 ID 类型</para>
+    /// <para>示例值：open_id</para>
+    /// <list type="bullet">
+    /// <item>open_id：标识一个用户在某个应用中的身份。同一个用户在不同应用中的 Open ID 不同。</item>
+    /// <item>union_id：标识一个用户在某个应用开发商下的身份。同一用户在同一开发商下的应用中的 Union ID 是相同的，在不同开发商下的应用中的 Union ID 是不同的。通过 Union ID，应用开发商可以把同个用户在多个应用中的身份关联起来。</item>
+    /// <item>user_id：标识一个用户在某个租户内的身份。同一个用户在租户 A 和租户 B 内的 User ID 是不同的。在同一个租户内，一个用户的 User ID 在所有应用（包括商店应用）中都保持一致。User ID 主要用于在不同的应用间打通用户数据。</item>
+    /// </list>
+    /// </param>
+    /// <param name="reserve_config_id">
+    /// <para>会议室或层级id</para>
+    /// <para>示例值：omm_3c5dd7e09bac0c1758fcf9511bd1a771</para>
+    /// </param>
+    /// <param name="updateReserveConfigFormRequest">更新会议室预定表单请求体</param>
+    /// <param name="cancellationToken"><see cref="CancellationToken"/>取消操作令牌对象。</param>
+    [Patch("/open-apis/vc/v1/reserve_configs/{reserve_config_id}/form")]
+    Task<FeishuNullDataApiResult?> UpdateReserveConfigFormAsync(
+       [Path] string reserve_config_id,
+       [Body] UpdateReserveConfigFormRequest updateReserveConfigFormRequest,
+       [Query] string? user_id_type = Consts.User_Id_Type,
+       CancellationToken cancellationToken = default);
 }

acb5dc961f29af10dcba73f9c4bffcdf2d2b4f82
倔强的石头 <413901125@qq.com>
2026-05-25 11:37:02 +0800

    feat(mail): 新增批量删除邮件会话的API支持
    新增批量删除用户邮箱会话的请求模型与相关接口定义，包含租户级和用户级的API接口，实现飞书邮件会话批量移至删除文件夹的功能

---
+++ Mud.Feishu/Interfaces/Mail/IFeishuV1MailThread.cs
@@ -0,0 +1,38 @@
+﻿// -----------------------------------------------------------------------
+//  作者：Mud Studio  版权所有 (c) Mud Studio 2026   
+//  Mud.Feishu 项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
+//  本项目主要遵循 MIT 许可证进行分发和使用。许可证位于源代码树根目录中的 LICENSE-MIT 文件。
+//  不得利用本项目从事危害国家安全、扰乱社会秩序、侵犯他人合法权益等法律法规禁止的活动！任何基于本项目开发而产生的一切法律纠纷和责任，我们不承担任何责任！
+// -----------------------------------------------------------------------
+
+using Mud.Feishu.DataModels.Mail;
+
+namespace Mud.Feishu.Interfaces;
+
+
+/// <summary>
+/// 飞书邮件会话API接口实现了修改、查询、删除等邮件模板功能。
+/// <para>接口详细文档请参见：<see href="https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/reference/mail-v1/user_mailbox-thread/batch_trash"/></para>
+/// </summary>
+[HttpClientApi(TokenManage = nameof(IFeishuAppManager), IsAbstract = true)]
+[Token("TenantAccessToken", Name = Consts.Authorization)]
+public interface IFeishuV1MailThread : IFeishuAppContextSwitcher
+{
+
+    /// <summary>
+    /// 批量删除邮件会话。
+    /// <para>批量将指定的邮件会话移入已删除文件夹。</para>
+    /// <para><see href="https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/reference/mail-v1/user_mailbox-thread/batch_trash">接口文档</see></para>
+    /// </summary>
+    /// <param name="user_mailbox_id">
+    /// <para>用户邮箱地址，作为用户邮箱身份标识。使用 user_access_token 调用时，可使用占位符 `me` 表示当前授权用户的主邮箱。</para>
+    /// <para>示例值：user@example.com</para>
+    /// </param>
+    /// <param name="batchTrashUserMailboxThreadRequest">批量删除邮件会话请求对象，包含待删除的邮件会话 ID 列表。</param>
+    /// <param name="cancellationToken"><see cref="CancellationToken"/>取消操作令牌对象。</param>
+    [Post("/open-apis/mail/v1/user_mailboxes/{user_mailbox_id}/threads/batch_trash")]
+    Task<FeishuNullDataApiResult?> BatchTrashUserMailboxThreadAsync(
+      [Path] string user_mailbox_id,
+      [Body] BatchTrashUserMailboxThreadRequest batchTrashUserMailboxThreadRequest,
+      CancellationToken cancellationToken = default);
+}

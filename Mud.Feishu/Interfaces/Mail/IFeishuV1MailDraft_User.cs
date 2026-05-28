f1556814d201e20ee3931570c6ad5cd2a3cb90f0
倔强的石头 <413901125@qq.com>
2026-05-28 15:46:22 +0800

    feat(mail): add paging draft list api for user mailbox
    add draft id response model and list draft api interface for feishu mail draft

--- Mud.Feishu/Interfaces/Mail/IFeishuV1MailDraft_User.cs
+++ Mud.Feishu/Interfaces/Mail/IFeishuV1MailDraft_User.cs
@@ -62,4 +62,24 @@ public interface IFeishuUserV1MailDraft : IFeishuAppContextSwitcher, ICurrentUse
       [Path] string user_mailbox_id,
       [Path] string draft_id,
       CancellationToken cancellationToken = default);
+
+
+    /// <summary>
+    /// 分页列出草稿列表。
+    /// <para>分页列出用户草稿箱中的草稿，只会返回草稿ID信息，不会返回草稿内容。</para>
+    /// <para><see href="https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/reference/mail-v1/user_mailbox-draft/list">接口文档</see></para>
+    /// </summary>
+    /// <param name="user_mailbox_id">
+    /// <para>用户邮箱地址，作为用户邮箱身份标识。使用 user_access_token 调用时，可使用占位符 `me` 表示当前授权用户的主邮箱。</para>
+    /// <para>示例值：user@example.com</para>
+    /// </param>
+    /// <param name="page_size">分页大小，即本次请求所返回的信息列表内的最大条目数。默认值：20</param>
+    /// <param name="page_token">分页标记，第一次请求不填，表示从头开始遍历；分页查询结果还有更多项时会同时返回新的 page_token，下次遍历可采用该 page_token 获取查询结果</param>
+    /// <param name="cancellationToken"><see cref="CancellationToken"/>取消操作令牌对象。</param>
+    [Get("/open-apis/mail/v1/user_mailboxes/{user_mailbox_id}/drafts")]
+    Task<FeishuApiPageListResult<DraftId>?> GetUserMailboxDraftPageListAsync(
+        [Path] string user_mailbox_id,
+        [Query] int page_size = Consts.PageSize_20,
+        [Query] string? page_token = null,
+        CancellationToken cancellationToken = default);
 }

a77beac601e37756b287820f6d1cfee488d8c534
倔强的石头 <413901125@qq.com>
2026-05-28 17:55:07 +0800

    feat(mail): add飞书邮箱文件夹相关API和数据模型
    新增飞书邮箱文件夹的接口定义、租户级/用户级API实现以及对应的数据模型，同时优化了邮件会话接口的注释描述。

---
+++ Mud.Feishu/Interfaces/Mail/IFeishuV1MailFolder.cs
@@ -0,0 +1,42 @@
+﻿// -----------------------------------------------------------------------
+//  作者：Mud Studio  版权所有 (c) Mud Studio 2026   
+//  Mud.Feishu 项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
+//  本项目主要遵循 MIT 许可证进行分发和使用。许可证位于源代码树根目录中的 LICENSE-MIT 文件。
+//  不得利用本项目从事危害国家安全、扰乱社会秩序、侵犯他人合法权益等法律法规禁止的活动！任何基于本项目开发而产生的一切法律纠纷和责任，我们不承担任何责任！
+// -----------------------------------------------------------------------
+
+using Mud.Feishu.DataModels.Mail;
+
+namespace Mud.Feishu.Interfaces;
+
+
+/// <summary>
+/// 飞书邮箱文件夹API接口实现了修改、查询、删除等邮箱文件夹管理功能。
+/// <para>接口详细文档请参见：<see href="https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/reference/mail-v1/user_mailbox-folder/get"/></para>
+/// </summary>
+[HttpClientApi(TokenManage = nameof(IFeishuAppManager), IsAbstract = true)]
+[Token("TenantAccessToken", Name = Consts.Authorization)]
+public interface IFeishuV1MailFolder : IFeishuAppContextSwitcher
+{
+
+
+    /// <summary>
+    /// 获取邮箱文件信息。
+    /// <para>通过指定文件夹ID，获取文件夹信息，包括名称、类型等。</para>
+    /// <para><see href="https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/reference/mail-v1/user_mailbox-folder/get">接口文档</see></para>
+    /// </summary>
+    /// <param name="user_mailbox_id">
+    /// <para>用户邮箱地址，作为用户邮箱身份标识。使用 user_access_token 调用时，可使用占位符 `me` 表示当前授权用户的主邮箱。</para>
+    /// <para>示例值：user@example.com</para>
+    /// </param>
+    /// <param name="folder_id">
+    /// <para>邮件文件夹唯一标识。可通过「获取邮箱文件夹列表」接口获取目标文件夹的 ID；若未传入该参数，默认返回根文件夹（收件箱）详情。</para>
+    /// <para>示例值：7620095646711680541</para>
+    /// </param>
+    /// <param name="cancellationToken"><see cref="CancellationToken"/>取消操作令牌对象。</param>
+    [Get("/open-apis/mail/v1/user_mailboxes/{user_mailbox_id}/folders/{folder_id}")]
+    Task<FeishuApiResult<GetUserMailboxFoldeResult>?> GetUserMailboxFoldeAsync(
+      [Path] string user_mailbox_id,
+      [Path] string folder_id,
+      CancellationToken cancellationToken = default);
+}

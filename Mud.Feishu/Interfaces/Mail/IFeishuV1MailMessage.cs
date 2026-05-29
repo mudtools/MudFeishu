82daad51300b84825ebb4a6958eafeae5e902102
倔强的石头 <413901125@qq.com>
2026-05-29 10:33:49 +0800

    feat(mail): add get user mailbox message api and model
    add GetUserMailboxMessageResult response model and add the corresponding get user mailbox message interface method

--- Mud.Feishu/Interfaces/Mail/IFeishuV1MailMessage.cs
+++ Mud.Feishu/Interfaces/Mail/IFeishuV1MailMessage.cs
@@ -236,5 +236,24 @@ public interface IFeishuV1MailMessage : IFeishuAppContextSwitcher
         CancellationToken cancellationToken = default);
 
 
-
+    /// <summary>
+    /// 修改邮件。
+    /// <para>修改邮件标签、所属文件夹、已读未读状态，可为邮件添加旗标、归档、移入垃圾邮件等操作。不支持移动邮件到已删除文件夹，如需，请使用删除邮件接口。</para>
+    /// <para><see href="https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/reference/mail-v1/user_mailbox-message/modify">接口文档</see></para>
+    /// </summary>
+    /// <param name="user_mailbox_id">
+    /// <para>用户邮箱地址，作为用户邮箱身份标识。使用 user_access_token 调用时，可使用占位符 `me` 表示当前授权用户的主邮箱。</para>
+    /// <para>示例值：user@example.com</para>
+    /// </param>
+    /// <param name="message_id">
+    /// <para>邮件ID，可通过列出邮件接口获得</para>
+    /// <para>示例值：NzR3Zkd5NGhBTS9NVkZnSklidDVGT3VoQmM4PQ==</para>
+    /// </param>
+    /// <param name="cancellationToken"><see cref="CancellationToken"/>取消操作令牌对象。</param>
+    [Get("/open-apis/mail/v1/user_mailboxes/{user_mailbox_id}/messages/{message_id}")]
+    Task<FeishuApiResult<GetUserMailboxMessageResult>?> GetUserMailboxMessageAsync(
+         [Path] string user_mailbox_id,
+         [Path] string message_id,
+         [Query] string? format = null,
+         CancellationToken cancellationToken = default);
 }

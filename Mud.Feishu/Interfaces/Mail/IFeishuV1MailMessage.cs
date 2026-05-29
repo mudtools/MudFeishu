1d19b376cb4b2b253c83a4d71b68055a5961bde6
倔强的石头 <413901125@qq.com>
2026-05-29 09:26:14 +0800

    feat(mail): add batch modify mail and single delete mail api support
    add BatchModifyUserMailboxMessageRequest model and implement two new mail apis: batch modify mails and single delete mail, reference feishu open platform docs

--- Mud.Feishu/Interfaces/Mail/IFeishuV1MailMessage.cs
+++ Mud.Feishu/Interfaces/Mail/IFeishuV1MailMessage.cs
@@ -35,4 +35,45 @@ public interface IFeishuV1MailMessage : IFeishuAppContextSwitcher
          [Path] string user_mailbox_id,
          [Body] BatchTrashUserMailboxMessageRequest request,
          CancellationToken cancellationToken = default);
+
+
+
+    /// <summary>
+    /// 批量修改邮件。
+    /// <para>批量修改邮件标签、所属文件夹、已读未读状态，可进行加旗标、归档、移至垃圾邮件等操作。不支持移入邮件进入已删除文件夹，如需，请使用批量删除邮件接口。</para>
+    /// <para><see href="https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/reference/mail-v1/user_mailbox-message/batch_modify">接口文档</see></para>
+    /// </summary>
+    /// <param name="user_mailbox_id">
+    /// <para>用户邮箱地址，作为用户邮箱身份标识。使用 user_access_token 调用时，可使用占位符 `me` 表示当前授权用户的主邮箱。</para>
+    /// <para>示例值：user@example.com</para>
+    /// </param>
+    /// <param name="request">批量修改用户邮箱邮件请求对象，包含待修改的邮件ID列表和需要修改的属性信息。</param>
+    /// <param name="cancellationToken"><see cref="CancellationToken"/>取消操作令牌对象。</param>
+    [Post("/open-apis/mail/v1/user_mailboxes/{user_mailbox_id}/messages/batch_modify")]
+    Task<FeishuNullDataApiResult?> BatchModifyUserMailboxMessageAsync(
+        [Path] string user_mailbox_id,
+        [Body] BatchModifyUserMailboxMessageRequest request,
+        CancellationToken cancellationToken = default);
+
+
+
+    /// <summary>
+    /// 删除邮件。
+    /// <para>移动邮件到已删除文件夹。</para>
+    /// <para><see href="https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/reference/mail-v1/user_mailbox-message/trash">接口文档</see></para>
+    /// </summary>
+    /// <param name="user_mailbox_id">
+    /// <para>用户邮箱地址，作为用户邮箱身份标识。使用 user_access_token 调用时，可使用占位符 `me` 表示当前授权用户的主邮箱。</para>
+    /// <para>示例值：user@example.com</para>
+    /// </param>
+    /// <param name="message_id">
+    /// <para>邮件ID，可通过列出邮件接口获得</para>
+    /// <para>示例值：NzR3Zkd5NGhBTS9NVkZnSklidDVGT3VoQmM4PQ==</para>
+    /// </param>
+    /// <param name="cancellationToken"><see cref="CancellationToken"/>取消操作令牌对象。</param>
+    [Post("/open-apis/mail/v1/user_mailboxes/{user_mailbox_id}/messages/{message_id}/trash")]
+    Task<FeishuNullDataApiResult?> DeleteUserMailboxMessageAsync(
+       [Path] string user_mailbox_id,
+       [Path] string message_id,
+       CancellationToken cancellationToken = default);
 }

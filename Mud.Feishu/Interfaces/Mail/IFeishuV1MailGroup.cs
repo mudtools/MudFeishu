bb538c778ceee3765b1ad9a463b0824bb52b9a86
倔强的石头 <413901125@qq.com>
2026-06-01 18:32:54 +0800

    feat(mail): add mail group member related APIs and data models
    新增飞书邮件组管理相关的请求/响应数据模型，以及完整的邮件组成员增删查改和批量操作API接口，包括创建、删除、获取单个成员、分页获取成员列表、批量创建和批量删除邮件组成员功能。

--- Mud.Feishu/Interfaces/Mail/IFeishuV1MailGroup.cs
+++ Mud.Feishu/Interfaces/Mail/IFeishuV1MailGroup.cs
@@ -217,4 +217,207 @@ public interface IFeishuTenantV1MailGroup : IFeishuAppContextSwitcher
          [Query] int page_size = Consts.PageSize_20,
          [Query] string? page_token = null,
          CancellationToken cancellationToken = default);
+
+
+
+    /// <summary>
+    /// 创建邮件组成员。
+    /// <para>向邮件组添加单个成员。</para>
+    /// <para><see href="https://open.feishu.cn/document/server-docs/mail-v1/mail-group/mailgroup-member/create">接口文档</see></para>
+    /// </summary>
+    /// <param name="mailgroup_id">
+    /// <para>邮件组ID或邮箱地址</para>
+    /// <para>示例值：xxxxxx 或 test_mail_group@xx.xx</para>
+    /// </param>
+    /// <param name="user_id_type">
+    /// <para>用户 ID 类型</para>
+    /// <para>示例值：open_id</para>
+    /// <list type="bullet">
+    /// <item>open_id：标识一个用户在某个应用中的身份。同一个用户在不同应用中的 Open ID 不同。[了解更多：如何获取 Open ID](https://open.feishu.cn/document/uAjLw4CM/ugTN1YjL4UTN24CO1UjN/trouble-shooting/how-to-obtain-openid)</item>
+    /// <item>union_id：标识一个用户在某个应用开发商下的身份。同一用户在同一开发商下的应用中的 Union ID 是相同的，在不同开发商下的应用中的 Union ID 是不同的。通过 Union ID，应用开发商可以把同个用户在多个应用中的身份关联起来。[了解更多：如何获取 Union ID？](https://open.feishu.cn/document/uAjLw4CM/ugTN1YjL4UTN24CO1UjN/trouble-shooting/how-to-obtain-union-id)</item>
+    /// <item>user_id：标识一个用户在某个租户内的身份。同一个用户在租户 A 和租户 B 内的 User ID 是不同的。在同一个租户内，一个用户的 User ID 在所有应用（包括商店应用）中都保持一致。User ID 主要用于在不同的应用间打通用户数据。[了解更多：如何获取 User ID？](https://open.feishu.cn/document/uAjLw4CM/ugTN1YjL4UTN24CO1UjN/trouble-shooting/how-to-obtain-user-id)</item>
+    /// </list>
+    /// <para>默认值：open_id</para>
+    /// </param>
+    /// <param name="department_id_type">
+    /// <para>此次调用中使用的部门ID的类型</para>
+    /// <para>示例值：open_department_id</para>
+    /// <list type="bullet">
+    /// <item>department_id：以自定义department_id来标识部门</item>
+    /// <item>open_department_id：以open_department_id来标识部门</item>
+    /// </list>
+    /// <para>默认值：open_department_id</para>
+    /// </param>
+    /// <param name="request">添加邮件组成员请求对象，包含待添加的邮件组成员信息。</param>
+    /// <param name="cancellationToken"><see cref="CancellationToken"/>取消操作令牌对象。</param>
+    [Post("/open-apis/mail/v1/mailgroups/{mailgroup_id}/members")]
+    Task<FeishuApiResult<MailGroupMemberOopsResult>?> CreateMailGroupMemberAsync(
+           [Path] string mailgroup_id,
+           [Body] CreateMailGroupMemberRequest request,
+           [Query] string? user_id_type = Consts.User_Id_Type,
+           [Query] string? department_id_type = Consts.Department_Id_Type,
+           CancellationToken cancellationToken = default);
+
+
+    /// <summary>
+    /// 删除邮件组成员。
+    /// <para>删除邮件组单个成员。</para>
+    /// <para><see href="https://open.feishu.cn/document/server-docs/mail-v1/mail-group/mailgroup-member/delete">接口文档</see></para>
+    /// </summary>
+    /// <param name="mailgroup_id">
+    /// <para>邮件组ID或邮箱地址</para>
+    /// <para>示例值：xxxxxx 或 test_mail_group@xx.xx</para>
+    /// </param>
+    /// <param name="member_id">
+    /// <para>The unique ID of a member in this mail group</para>
+    /// <para>示例值：xxxxxxxxxxxxxxx</para>
+    /// </param>
+    /// <param name="cancellationToken"><see cref="CancellationToken"/>取消操作令牌对象。</param>
+    [Delete("/open-apis/mail/v1/mailgroups/{mailgroup_id}/members/{member_id}")]
+    Task<FeishuNullDataApiResult?> DeleteMailGroupMemberAsync(
+            [Path] string mailgroup_id,
+            [Path] string member_id,
+            CancellationToken cancellationToken = default);
+
+
+    /// <summary>
+    /// 查询指定邮件组成员。
+    /// <para>获取邮件组单个成员信息。</para>
+    /// <para><see href="https://open.feishu.cn/document/server-docs/mail-v1/mail-group/mailgroup-member/get">接口文档</see></para>
+    /// </summary>
+    /// <param name="mailgroup_id">
+    /// <para>邮件组ID或邮箱地址</para>
+    /// <para>示例值：xxxxxx 或 test_mail_group@xx.xx</para>
+    /// </param>
+    /// <param name="member_id">
+    /// <para>The unique ID of a member in this mail group</para>
+    /// <para>示例值：xxxxxxxxxxxxxxx</para>
+    /// </param>
+    /// <param name="user_id_type">
+    /// <para>用户 ID 类型</para>
+    /// <para>示例值：open_id</para>
+    /// <list type="bullet">
+    /// <item>open_id：标识一个用户在某个应用中的身份。同一个用户在不同应用中的 Open ID 不同。[了解更多：如何获取 Open ID](https://open.feishu.cn/document/uAjLw4CM/ugTN1YjL4UTN24CO1UjN/trouble-shooting/how-to-obtain-openid)</item>
+    /// <item>union_id：标识一个用户在某个应用开发商下的身份。同一用户在同一开发商下的应用中的 Union ID 是相同的，在不同开发商下的应用中的 Union ID 是不同的。通过 Union ID，应用开发商可以把同个用户在多个应用中的身份关联起来。[了解更多：如何获取 Union ID？](https://open.feishu.cn/document/uAjLw4CM/ugTN1YjL4UTN24CO1UjN/trouble-shooting/how-to-obtain-union-id)</item>
+    /// <item>user_id：标识一个用户在某个租户内的身份。同一个用户在租户 A 和租户 B 内的 User ID 是不同的。在同一个租户内，一个用户的 User ID 在所有应用（包括商店应用）中都保持一致。User ID 主要用于在不同的应用间打通用户数据。[了解更多：如何获取 User ID？](https://open.feishu.cn/document/uAjLw4CM/ugTN1YjL4UTN24CO1UjN/trouble-shooting/how-to-obtain-user-id)</item>
+    /// </list>
+    /// <para>默认值：open_id</para>
+    /// </param>
+    /// <param name="department_id_type">
+    /// <para>此次调用中使用的部门ID的类型</para>
+    /// <para>示例值：open_department_id</para>
+    /// <list type="bullet">
+    /// <item>department_id：以自定义department_id来标识部门</item>
+    /// <item>open_department_id：以open_department_id来标识部门</item>
+    /// </list>
+    /// <para>默认值：open_department_id</para>
+    /// </param>
+    /// <param name="cancellationToken"><see cref="CancellationToken"/>取消操作令牌对象。</param>
+    [Get("/open-apis/mail/v1/mailgroups/{mailgroup_id}/members/{member_id}")]
+    Task<FeishuApiResult<MailGroupMemberOopsResult>?> GetMailGroupMemberAsync(
+           [Path] string mailgroup_id,
+           [Path] string member_id,
+           [Query] string? user_id_type = Consts.User_Id_Type,
+           [Query] string? department_id_type = Consts.Department_Id_Type,
+           CancellationToken cancellationToken = default);
+
+
+    /// <summary>
+    /// 分页获取所有邮件组成员。
+    /// <para>分页批量获取邮件组成员列表。</para>
+    /// <para><see href="https://open.feishu.cn/document/server-docs/mail-v1/mail-group/mailgroup-member/list">接口文档</see></para>
+    /// </summary>
+    /// <param name="mailgroup_id">
+    /// <para>邮件组ID或邮箱地址</para>
+    /// <para>示例值：xxxxxx 或 test_mail_group@xx.xx</para>
+    /// </param>
+    /// <param name="user_id_type">
+    /// <para>用户 ID 类型</para>
+    /// <para>示例值：open_id</para>
+    /// <list type="bullet">
+    /// <item>open_id：标识一个用户在某个应用中的身份。同一个用户在不同应用中的 Open ID 不同。[了解更多：如何获取 Open ID](https://open.feishu.cn/document/uAjLw4CM/ugTN1YjL4UTN24CO1UjN/trouble-shooting/how-to-obtain-openid)</item>
+    /// <item>union_id：标识一个用户在某个应用开发商下的身份。同一用户在同一开发商下的应用中的 Union ID 是相同的，在不同开发商下的应用中的 Union ID 是不同的。通过 Union ID，应用开发商可以把同个用户在多个应用中的身份关联起来。[了解更多：如何获取 Union ID？](https://open.feishu.cn/document/uAjLw4CM/ugTN1YjL4UTN24CO1UjN/trouble-shooting/how-to-obtain-union-id)</item>
+    /// <item>user_id：标识一个用户在某个租户内的身份。同一个用户在租户 A 和租户 B 内的 User ID 是不同的。在同一个租户内，一个用户的 User ID 在所有应用（包括商店应用）中都保持一致。User ID 主要用于在不同的应用间打通用户数据。[了解更多：如何获取 User ID？](https://open.feishu.cn/document/uAjLw4CM/ugTN1YjL4UTN24CO1UjN/trouble-shooting/how-to-obtain-user-id)</item>
+    /// </list>
+    /// <para>默认值：open_id</para>
+    /// </param>
+    /// <param name="department_id_type">
+    /// <para>此次调用中使用的部门ID的类型</para>
+    /// <para>示例值：open_department_id</para>
+    /// <list type="bullet">
+    /// <item>department_id：以自定义department_id来标识部门</item>
+    /// <item>open_department_id：以open_department_id来标识部门</item>
+    /// </list>
+    /// <para>默认值：open_department_id</para>
+    /// </param>
+    /// <param name="page_size">分页大小，即本次请求所返回的信息列表内的最大条目数。默认值：20</param>
+    /// <param name="page_token">分页标记，第一次请求不填，表示从头开始遍历；分页查询结果还有更多项时会同时返回新的 page_token，下次遍历可采用该 page_token 获取查询结果</param>
+    /// <param name="cancellationToken"><see cref="CancellationToken"/>取消操作令牌对象。</param>
+    [Get("/open-apis/mail/v1/mailgroups/{mailgroup_id}/members")]
+    Task<FeishuApiPageListResult<MailGroupMemberInfo>?> GetMailGroupMemberPageListAsync(
+         [Path] string mailgroup_id,
+         [Query] int page_size = Consts.PageSize_20,
+         [Query] string? page_token = null,
+         [Query] string? user_id_type = Consts.User_Id_Type,
+         [Query] string? department_id_type = Consts.Department_Id_Type,
+         CancellationToken cancellationToken = default);
+
+
+
+    /// <summary>
+    /// 批量创建邮件组成员。
+    /// <para>一次请求可以给一个邮件组添加多个成员。</para>
+    /// <para><see href="https://open.feishu.cn/document/server-docs/mail-v1/mail-group/mailgroup-member/batch_create">接口文档</see></para>
+    /// </summary>
+    /// <param name="mailgroup_id">
+    /// <para>邮件组ID或邮箱地址</para>
+    /// <para>示例值：xxxxxx 或 test_mail_group@xx.xx</para>
+    /// </param>
+    /// <param name="user_id_type">
+    /// <para>用户 ID 类型</para>
+    /// <para>示例值：open_id</para>
+    /// <list type="bullet">
+    /// <item>open_id：标识一个用户在某个应用中的身份。同一个用户在不同应用中的 Open ID 不同。[了解更多：如何获取 Open ID](https://open.feishu.cn/document/uAjLw4CM/ugTN1YjL4UTN24CO1UjN/trouble-shooting/how-to-obtain-openid)</item>
+    /// <item>union_id：标识一个用户在某个应用开发商下的身份。同一用户在同一开发商下的应用中的 Union ID 是相同的，在不同开发商下的应用中的 Union ID 是不同的。通过 Union ID，应用开发商可以把同个用户在多个应用中的身份关联起来。[了解更多：如何获取 Union ID？](https://open.feishu.cn/document/uAjLw4CM/ugTN1YjL4UTN24CO1UjN/trouble-shooting/how-to-obtain-union-id)</item>
+    /// <item>user_id：标识一个用户在某个租户内的身份。同一个用户在租户 A 和租户 B 内的 User ID 是不同的。在同一个租户内，一个用户的 User ID 在所有应用（包括商店应用）中都保持一致。User ID 主要用于在不同的应用间打通用户数据。[了解更多：如何获取 User ID？](https://open.feishu.cn/document/uAjLw4CM/ugTN1YjL4UTN24CO1UjN/trouble-shooting/how-to-obtain-user-id)</item>
+    /// </list>
+    /// <para>默认值：open_id</para>
+    /// </param>
+    /// <param name="department_id_type">
+    /// <para>此次调用中使用的部门ID的类型</para>
+    /// <para>示例值：open_department_id</para>
+    /// <list type="bullet">
+    /// <item>department_id：以自定义department_id来标识部门</item>
+    /// <item>open_department_id：以open_department_id来标识部门</item>
+    /// </list>
+    /// <para>默认值：open_department_id</para>
+    /// </param>
+    /// <param name="request">批量创建邮件组成员请求体</param>
+    /// <param name="cancellationToken"><see cref="CancellationToken"/>取消操作令牌对象。</param>
+    [Post("/open-apis/mail/v1/mailgroups/{mailgroup_id}/members/batch_create")]
+    Task<FeishuApiResult<BatchCreateMailGroupMemberResult>?> BatchCreateMailGroupMemberAsync(
+          [Path] string mailgroup_id,
+          [Body] BatchCreateMailGroupMemberRequest request,
+          [Query] string? user_id_type = Consts.User_Id_Type,
+          [Query] string? department_id_type = Consts.Department_Id_Type,
+          CancellationToken cancellationToken = default);
+
+
+
+    /// <summary>
+    /// 批量删除邮件组成员。
+    /// <para>一次请求可以删除一个邮件组中的多个成员。</para>
+    /// <para><see href="https://open.feishu.cn/document/server-docs/mail-v1/mail-group/mailgroup-member/batch_delete">接口文档</see></para>
+    /// </summary>
+    /// <param name="mailgroup_id">
+    /// <para>邮件组ID或邮箱地址</para>
+    /// <para>示例值：xxxxxx 或 test_mail_group@xx.xx</para>
+    /// </param>
+    /// <param name="request">批量删除邮件组成员请求体，包含待删除的邮件组成员信息。</param>
+    /// <param name="cancellationToken"><see cref="CancellationToken"/>取消操作令牌对象。</param>
+    [Delete("/open-apis/mail/v1/mailgroups/{mailgroup_id}/members/batch_delete")]
+    Task<FeishuNullDataApiResult?> BatchDeleteMailGroupMemberAsync(
+         [Path] string mailgroup_id,
+         [Body] BatchDeleteMailGroupMemberRequest request,
+         CancellationToken cancellationToken = default);
 }

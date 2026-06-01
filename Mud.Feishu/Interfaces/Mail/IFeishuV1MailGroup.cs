5f6b931f359a679832d633399ad5005e4208b8e4
倔强的石头 <413901125@qq.com>
2026-06-01 15:43:33 +0800

    feat(mail): add mailgroup manager batch operation apis
    add mailgroup manager entity, batch request model and 3 apis: batch create, batch delete and list mailgroup managers

--- Mud.Feishu/Interfaces/Mail/IFeishuV1MailGroup.cs
+++ Mud.Feishu/Interfaces/Mail/IFeishuV1MailGroup.cs
@@ -128,4 +128,93 @@ public interface IFeishuTenantV1MailGroup : IFeishuAppContextSwitcher
           [Query] int page_size = Consts.PageSize_20,
           [Query] string? page_token = null,
           CancellationToken cancellationToken = default);
+
+
+    /// <summary>
+    /// 批量创建邮件组管理员。
+    /// <para>批量创建邮件组管理员。</para>
+    /// <para><see href="https://open.feishu.cn/document/server-docs/mail-v1/mail-group/mailgroup-manager/batch_create">接口文档</see></para>
+    /// </summary>
+    /// <param name="mailgroup_id">
+    /// <para>邮件组ID或邮箱地址</para>
+    /// <para>示例值：xxxxxx 或 test_mail_group@xx.xx</para>
+    /// </param>
+    /// <param name="user_id_type">
+    /// <para>用户 ID 类型</para>
+    /// <para>示例值：open_id</para>
+    /// <list type="bullet">
+    /// <item>open_id：标识一个用户在某个应用中的身份。同一个用户在不同应用中的 Open ID 不同。[了解更多：如何获取 Open ID](https://open.feishu.cn/document/uAjLw4CM/ugTN1YjL4UTN24CO1UjN/trouble-shooting/how-to-obtain-openid)</item>
+    /// <item>union_id：标识一个用户在某个应用开发商下的身份。同一用户在同一开发商下的应用中的 Union ID 是相同的，在不同开发商下的应用中的 Union ID 是不同的。通过 Union ID，应用开发商可以把同个用户在多个应用中的身份关联起来。[了解更多：如何获取 Union ID？](https://open.feishu.cn/document/uAjLw4CM/ugTN1YjL4UTN24CO1UjN/trouble-shooting/how-to-obtain-union-id)</item>
+    /// <item>user_id：标识一个用户在某个租户内的身份。同一个用户在租户 A 和租户 B 内的 User ID 是不同的。在同一个租户内，一个用户的 User ID 在所有应用（包括商店应用）中都保持一致。User ID 主要用于在不同的应用间打通用户数据。[了解更多：如何获取 User ID？](https://open.feishu.cn/document/uAjLw4CM/ugTN1YjL4UTN24CO1UjN/trouble-shooting/how-to-obtain-user-id)</item>
+    /// </list>
+    /// <para>默认值：open_id</para>
+    /// </param>
+    /// <param name="request">批量添加邮件组管理员请求对象，包含待添加的邮件组管理员信息。</param>
+    /// <param name="cancellationToken"><see cref="CancellationToken"/>取消操作令牌对象。</param>
+    [Post("/open-apis/mail/v1/mailgroups/{mailgroup_id}/managers/batch_create")]
+    Task<FeishuNullDataApiResult?> BatchCreateMailgroupManagerAsync(
+         [Path] string mailgroup_id,
+         [Body] BatchOopsMailgroupManagerRequest request,
+         [Query] string? user_id_type = Consts.User_Id_Type,
+         CancellationToken cancellationToken = default);
+
+
+    /// <summary>
+    /// 批量删除邮件组管理员。
+    /// <para>批量删除邮件组管理员。</para>
+    /// <para><see href="https://open.feishu.cn/document/server-docs/mail-v1/mail-group/mailgroup-manager/batch_delete">接口文档</see></para>
+    /// </summary>
+    /// <param name="mailgroup_id">
+    /// <para>邮件组ID或邮箱地址</para>
+    /// <para>示例值：xxxxxx 或 test_mail_group@xx.xx</para>
+    /// </param>
+    /// <param name="user_id_type">
+    /// <para>用户 ID 类型</para>
+    /// <para>示例值：open_id</para>
+    /// <list type="bullet">
+    /// <item>open_id：标识一个用户在某个应用中的身份。同一个用户在不同应用中的 Open ID 不同。[了解更多：如何获取 Open ID](https://open.feishu.cn/document/uAjLw4CM/ugTN1YjL4UTN24CO1UjN/trouble-shooting/how-to-obtain-openid)</item>
+    /// <item>union_id：标识一个用户在某个应用开发商下的身份。同一用户在同一开发商下的应用中的 Union ID 是相同的，在不同开发商下的应用中的 Union ID 是不同的。通过 Union ID，应用开发商可以把同个用户在多个应用中的身份关联起来。[了解更多：如何获取 Union ID？](https://open.feishu.cn/document/uAjLw4CM/ugTN1YjL4UTN24CO1UjN/trouble-shooting/how-to-obtain-union-id)</item>
+    /// <item>user_id：标识一个用户在某个租户内的身份。同一个用户在租户 A 和租户 B 内的 User ID 是不同的。在同一个租户内，一个用户的 User ID 在所有应用（包括商店应用）中都保持一致。User ID 主要用于在不同的应用间打通用户数据。[了解更多：如何获取 User ID？](https://open.feishu.cn/document/uAjLw4CM/ugTN1YjL4UTN24CO1UjN/trouble-shooting/how-to-obtain-user-id)</item>
+    /// </list>
+    /// <para>默认值：open_id</para>
+    /// </param>
+    /// <param name="request">批量删除邮件组管理员请求对象，包含待删除的邮件组管理员信息。</param>
+    /// <param name="cancellationToken"><see cref="CancellationToken"/>取消操作令牌对象。</param>
+    [Post("/open-apis/mail/v1/mailgroups/{mailgroup_id}/managers/batch_delete")]
+    Task<FeishuNullDataApiResult?> BatchDeleteMailgroupManagerAsync(
+        [Path] string mailgroup_id,
+        [Body] BatchOopsMailgroupManagerRequest request,
+        [Query] string? user_id_type = Consts.User_Id_Type,
+        CancellationToken cancellationToken = default);
+
+
+    /// <summary>
+    /// 批量获取邮件组管理员。
+    /// <para>批量获取邮件组管理员。</para>
+    /// <para><see href="https://open.feishu.cn/document/server-docs/mail-v1/mail-group/mailgroup-manager/list">接口文档</see></para>
+    /// </summary>
+    /// <param name="mailgroup_id">
+    /// <para>邮件组ID或邮箱地址</para>
+    /// <para>示例值：xxxxxx 或 test_mail_group@xx.xx</para>
+    /// </param>
+    /// <param name="user_id_type">
+    /// <para>用户 ID 类型</para>
+    /// <para>示例值：open_id</para>
+    /// <list type="bullet">
+    /// <item>open_id：标识一个用户在某个应用中的身份。同一个用户在不同应用中的 Open ID 不同。[了解更多：如何获取 Open ID](https://open.feishu.cn/document/uAjLw4CM/ugTN1YjL4UTN24CO1UjN/trouble-shooting/how-to-obtain-openid)</item>
+    /// <item>union_id：标识一个用户在某个应用开发商下的身份。同一用户在同一开发商下的应用中的 Union ID 是相同的，在不同开发商下的应用中的 Union ID 是不同的。通过 Union ID，应用开发商可以把同个用户在多个应用中的身份关联起来。[了解更多：如何获取 Union ID？](https://open.feishu.cn/document/uAjLw4CM/ugTN1YjL4UTN24CO1UjN/trouble-shooting/how-to-obtain-union-id)</item>
+    /// <item>user_id：标识一个用户在某个租户内的身份。同一个用户在租户 A 和租户 B 内的 User ID 是不同的。在同一个租户内，一个用户的 User ID 在所有应用（包括商店应用）中都保持一致。User ID 主要用于在不同的应用间打通用户数据。[了解更多：如何获取 User ID？](https://open.feishu.cn/document/uAjLw4CM/ugTN1YjL4UTN24CO1UjN/trouble-shooting/how-to-obtain-user-id)</item>
+    /// </list>
+    /// <para>默认值：open_id</para>
+    /// </param>
+    /// <param name="page_size">分页大小，即本次请求所返回的信息列表内的最大条目数。默认值：20</param>
+    /// <param name="page_token">分页标记，第一次请求不填，表示从头开始遍历；分页查询结果还有更多项时会同时返回新的 page_token，下次遍历可采用该 page_token 获取查询结果</param>
+    /// <param name="cancellationToken"><see cref="CancellationToken"/>取消操作令牌对象。</param>
+    [Get("/open-apis/mail/v1/mailgroups/{mailgroup_id}/managers")]
+    Task<FeishuApiPageListResult<MailgroupManager>?> GetMailgroupManagerPageListAsync(
+         [Path] string mailgroup_id,
+         [Query] string? user_id_type = Consts.User_Id_Type,
+         [Query] int page_size = Consts.PageSize_20,
+         [Query] string? page_token = null,
+         CancellationToken cancellationToken = default);
 }

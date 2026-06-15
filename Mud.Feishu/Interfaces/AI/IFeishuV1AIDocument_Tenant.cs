4a60022c1bf4ba9bdc86b549451a35190f8c9ca6
倔强的石头 <413901125@qq.com>
2026-06-15 09:25:00 +0800

    feat(ai-document): add Taiwan travel permit recognize api support
    add all required data models and api interface for recognizing Taiwan residents' travel permit to mainland, including entity model, result model and api client method

--- Mud.Feishu/Interfaces/AI/IFeishuV1AIDocument_Tenant.cs
+++ Mud.Feishu/Interfaces/AI/IFeishuV1AIDocument_Tenant.cs
@@ -43,4 +43,17 @@ public interface IFeishuTenantV1AIDocument : IFeishuV1AIDocument
     Task<FeishuApiResult<RecognizeHkmMainlandTravelPermitResult>?> RecognizeHkmMainlandTravelPermitAsync(
       [FormContent] FileUploadRequest request,
       CancellationToken cancellationToken = default);
+
+
+    /// <summary>
+    /// 识别文件中的台湾居民来往大陆通行证信息。
+    /// <para>台湾居民来往大陆通行证识别接口，支持PDF/DOCX/PNG/JPG四种文件类型的一次性的识别。文件大小需要小于30M。</para>
+    /// <para><see href="https://open.feishu.cn/document/ai/document_ai-v1/tw_mainland_travel_permit/recognize">接口文档</see></para>
+    /// </summary>
+    /// <param name="request">上传用于AI处理的文件请求体</param>
+    /// <param name="cancellationToken"><see cref="CancellationToken"/>取消操作令牌对象。</param>
+    [Post("/open-apis/document_ai/v1/tw_mainland_travel_permit/recognize")]
+    Task<FeishuApiResult<RecognizeTwMainlandTravelPermitResult>?> RecognizeTwMainlandTravelPermitAsync(
+      [FormContent] FileUploadRequest request,
+      CancellationToken cancellationToken = default);
 }

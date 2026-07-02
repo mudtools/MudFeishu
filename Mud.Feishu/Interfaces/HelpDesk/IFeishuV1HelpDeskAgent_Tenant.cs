07732eae1d5669be8c1e6b4855562236a29cf8e6
倔强的石头 <413901125@qq.com>
2026-07-02 11:06:18 +0800

    feat(helpdesk): 新增客服管理相关接口和数据模型
    新增更新客服信息请求体、获取客服邮箱响应体数据模型，同时添加获取客服邮箱和更新客服信息的接口定义

--- Mud.Feishu/Interfaces/HelpDesk/IFeishuV1HelpDeskAgent_Tenant.cs
+++ Mud.Feishu/Interfaces/HelpDesk/IFeishuV1HelpDeskAgent_Tenant.cs
@@ -5,6 +5,8 @@
 //  不得利用本项目从事危害国家安全、扰乱社会秩序、侵犯他人合法权益等法律法规禁止的活动！任何基于本项目开发而产生的一切法律纠纷和责任，我们不承担任何责任！
 // -----------------------------------------------------------------------
 
+using Mud.Feishu.DataModels.HelpDesk;
+
 namespace Mud.Feishu;
 
 /// <summary>
@@ -16,4 +18,13 @@ namespace Mud.Feishu;
 public interface IFeishuTenantV1HelpDeskAgent : IFeishuV1HelpDeskAgent
 {
 
+    /// <summary>
+    /// 获取客服邮箱
+    /// <para>用于获取客服邮箱地址。</para>
+    /// <para><see href="https://open.feishu.cn/document/server-docs/helpdesk-v1/agent-function/agent/agent_email">接口文档</see></para>
+    /// </summary> 
+    /// <param name="cancellationToken"><see cref="CancellationToken"/>取消操作令牌对象。</param>
+    [Get("/open-apis/helpdesk/v1/agent_emails")]
+    Task<FeishuApiResult<GetAgentEmailResult>?> GetAgentEmailAsync(
+        CancellationToken cancellationToken = default);
 }
--- Mud.Feishu/Interfaces/HelpDesk/IFeishuV1HelpDeskAgent_User.cs
+++ Mud.Feishu/Interfaces/HelpDesk/IFeishuV1HelpDeskAgent_User.cs
@@ -5,6 +5,8 @@
 //  不得利用本项目从事危害国家安全、扰乱社会秩序、侵犯他人合法权益等法律法规禁止的活动！任何基于本项目开发而产生的一切法律纠纷和责任，我们不承担任何责任！
 // -----------------------------------------------------------------------
 
+using Mud.Feishu.DataModels.HelpDesk;
+
 namespace Mud.Feishu;
 
 /// <summary>
@@ -16,4 +18,20 @@ namespace Mud.Feishu;
 public interface IFeishuUserV1HelpDeskAgent : IFeishuV1HelpDeskAgent, ICurrentUserId
 {
 
+    /// <summary>
+    /// 更新客服信息
+    /// <para>更新客服状态等信息。</para>
+    /// <para><see href="https://open.feishu.cn/document/server-docs/helpdesk-v1/agent-function/agent/patch">接口文档</see></para>
+    /// </summary> 
+    /// <param name="agent_id">
+    /// <para>客服id</para>
+    /// <para>示例值：ou_14777d82ffef0f707de5a8c7ff2c5ebe</para>
+    /// </param>
+    /// <param name="request">更新客服信息请求体</param>
+    /// <param name="cancellationToken"><see cref="CancellationToken"/>取消操作令牌对象。</param>
+    [Patch("/open-apis/helpdesk/v1/agents/{agent_id}")]
+    Task<FeishuNullDataApiResult?> UpdateAgentInfoAsync(
+        [Path] string agent_id,
+        [Body] UpdateAgentInfoRequest request,
+        CancellationToken cancellationToken = default);
 }
